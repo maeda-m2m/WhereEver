e2af0645000cfd44978c83bccac68c76a30136a3
sakaguchi-m2m <sakaguchi@m2m-asp.com>
2021-04-20 18:15:56 +0900

    緊急

--- ファイル共有/FileShare.aspx.cs
+++ ファイル共有/FileShare.aspx.cs
@@ -248,7 +248,7 @@ namespace WhereEver
             {
                 //GridView1の色を変えた色をもとに戻す
                 int resetargs = int.Parse(Session["args"].ToString());
-                GridView1.Rows[resetargs].BackColor = System.Drawing.Color.Empty;
+                GridView_a.Rows[resetargs].BackColor = System.Drawing.Color.Empty;
             }
 
             //新たに色を変更する行を記憶
@@ -261,7 +261,7 @@ namespace WhereEver
             //string isbn_name = GridView1.Rows[args].Cells[0].Text.Trim();
 
             //FileNameをロード
-            string isbn_filename = GridView1.Rows[args].Cells[1].Text.Trim();
+            string isbn_filename = GridView_a.Rows[args].Cells[1].Text.Trim();
 
             lbluid.Text = isbn_filename;
             TextBox_dl.Text = isbn_filename;
@@ -306,10 +306,10 @@ namespace WhereEver
             {
 
                 //この方式では複数ページが取得できない
-                int cnt = GridView1.Rows.Count;
+                int cnt = GridView_a.Rows.Count;
 
                 //ページを複数表示したい場合（仮実装）
-                //int page = GridView1.PageCount;
+                //int page = GridView_a.PageCount;
                 //cnt *= page;
 
                 try
@@ -320,13 +320,13 @@ namespace WhereEver
                     {
                         //Rows[i]行の左から2番目の列[0-nで数える]のセルにある「テキスト」を取得
                         //uidをロード
-                        string isbn_uid = GridView1.Rows[i].Cells[1].Text.Trim();
+                        string isbn_uid = GridView_a.Rows[i].Cells[1].Text.Trim();
 
                         //uidは一致したか？
                         if (lbluid.Text == isbn_uid)
                         {
                             //GridView1におけるuidと一致する行の背景に色を付けて強調表示する
-                            GridView1.Rows[i].BackColor = System.Drawing.Color.AliceBlue;
+                            GridView_a.Rows[i].BackColor = System.Drawing.Color.AliceBlue;
                             break;
                         }
 
