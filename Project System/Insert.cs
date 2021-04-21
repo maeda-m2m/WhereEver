fe83599773b5d841fe0637431bfac860941825d4
chou-m2m <chou@m2m-asp.com>
2021-04-21 11:43:03 +0900

    chou Pkanri

--- Project System/Insert.cs
+++ Project System/Insert.cs
@@ -8,6 +8,44 @@ namespace WhereEver.Project_System
 {
     public class Insert
     {
+        internal static void InsertPBig(DATASET.DataSet.T_PdbKanriDataTable dt, SqlConnection sqlConnection)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("", sqlConnection);
+            da.SelectCommand.CommandText =
+                "SELECT * FROM T_PdbKanri";
+            da.InsertCommand = (new SqlCommandBuilder(da)).GetInsertCommand();
+
+            SqlTransaction sql;
+
+            try
+            {
+                sqlConnection.Open();
+                sql = sqlConnection.BeginTransaction();
+
+                da.SelectCommand.Transaction = da.InsertCommand.Transaction = sql;
+
+                da.Update(dt);
+
+                sql.Commit();
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+        internal static DATASET.DataSet.T_PdbKanriRow GetMaxPBigidRow(SqlConnection sqlConnection)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("", sqlConnection);
+            da.SelectCommand.CommandText =
+                "select MAX(PBigid) as PBigid from T_PdbKanri";
+            DATASET.DataSet.T_PdbKanriDataTable dt = new DATASET.DataSet.T_PdbKanriDataTable();
+            da.Fill(dt);
+            if(dt[0] == null)
+            {
+                dt[0].PBigid = 0;
+            }
+            return dt[0];
+        }
         internal static DATASET.DataSet.T_PdbRow GetMaxPidRow(SqlConnection sqlConnection)
         {
             SqlDataAdapter da = new SqlDataAdapter("", sqlConnection);
--- Project System/PKanri.aspx.cs
+++ Project System/PKanri.aspx.cs
@@ -13,5 +13,25 @@ namespace WhereEver.Project_System
         {
 
         }
+
+        protected void DgPKanri_ItemDataBound(object sender, DataGridItemEventArgs e)
+        {
+
+        }
+
+        protected void btnToroku_Click(object sender, EventArgs e)
+        {
+            DATASET.DataSet.T_PdbKanriDataTable t_PdbKanris = new DATASET.DataSet.T_PdbKanriDataTable();
+            DATASET.DataSet.T_PdbKanriRow t_PdbKanriRow = t_PdbKanris.NewT_PdbKanriRow();
+
+            DATASET.DataSet.T_PdbKanriRow t_PdbKanriRow1 = Insert.GetMaxPBigidRow(Global.GetConnection());
+
+            int sl = t_PdbKanriRow1.PBigid;
+            t_PdbKanriRow.PBigid = sl +1;
+            t_PdbKanriRow.PBigname = txtPBig.Text;
+
+            t_PdbKanris.Rows.Add(t_PdbKanriRow);
+            Insert.InsertPBig(t_PdbKanris, Global.GetConnection());
+        }
     }
 }
