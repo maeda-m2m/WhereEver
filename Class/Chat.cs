2c807ee34ce1728e0142a14b7a12398dc05c8cff
chou-m2m <chou@m2m-asp.com>
2021-04-28 14:14:53 +0900

    Chat

--- Class/Chat.cs
+++ Class/Chat.cs
@@ -12,7 +12,7 @@ namespace WhereEver.Class
         internal static DATASET.DataSet.T_ChatDataTable GetChatDataTable(SqlConnection sqlConnection)
         {
             SqlDataAdapter da = new SqlDataAdapter("", sqlConnection);
-            da.SelectCommand.CommandText = "select * from T_Chat Order by No";
+            da.SelectCommand.CommandText = "select * from T_Chat Order by No,HentouNo";
             DATASET.DataSet.T_ChatDataTable dt = new DataSet.T_ChatDataTable();
             da.Fill(dt);
             return (dt);
