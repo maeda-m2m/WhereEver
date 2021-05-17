60c90b90c77767e3c7c75373f2d7e99ca4d5d145
sakaguchi-m2m <sakaguchi@m2m-asp.com>
2021-05-17 17:30:12 +0900

    sakaguchi222

--- スケジュール/PrintSchedule.aspx.designer.cs
+++ スケジュール/PrintSchedule.aspx.designer.cs
@@ -1804,5 +1804,14 @@ namespace WhereEver
         /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblbikou;
+
+        /// <summary>
+        /// DgBikou コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataGrid DgBikou;
     }
 }
