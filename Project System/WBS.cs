4ba6ccd16b2bf735796c51ec2b257aa24d83ba7d
chou-m2m <chou@m2m-asp.com>
2021-05-19 13:48:22 +0900

    project 3.1

--- Project System/WBS.cs
+++ Project System/WBS.cs
@@ -8,14 +8,26 @@ namespace WhereEver.Project_System
 {
     public class WBS
     {
-        internal static DATASET.DataSet.T_PdbKanriRow GetPMiddleTimeRow(SqlConnection sqlConnection)
+        internal static DATASET.DataSet.T_PdbKanriRow GetPMiddleTimeRow(SqlConnection sqlConnection,int spId)
         {
             SqlDataAdapter da = new SqlDataAdapter("", sqlConnection);
             da.SelectCommand.CommandText =
-                "select MIN(PMiddlestart) as PMiddlestart , MAX(PMiddleover) as PMiddleover from T_PdbKanri";
+                "select MIN(PMiddlestart) as PMiddlestart , MAX(PMiddleover) as PMiddleover from T_PdbKanri where Pid = @spId";
+            da.SelectCommand.Parameters.AddWithValue("@spId", spId);
             DATASET.DataSet.T_PdbKanriDataTable dt = new DATASET.DataSet.T_PdbKanriDataTable();
             da.Fill(dt);
             return dt[0];
+        }
+
+        internal static int GetT_PdbKanriDataTable(SqlConnection sqlConnection, int spId)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("", sqlConnection);
+            da.SelectCommand.CommandText =
+                "select count(*) as count from T_PdbKanri where Pid = @spId";
+            da.SelectCommand.Parameters.AddWithValue("@spId", spId);
+            DATASET.DataSet.T_PdbKanriDataTable dt = new DATASET.DataSet.T_PdbKanriDataTable();
+            da.Fill(dt);
+            return (int)dt[0][9];
         }
     }
 }
